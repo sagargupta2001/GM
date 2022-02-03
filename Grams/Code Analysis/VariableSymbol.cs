97f465bf8ddb3d83338c040c5e400240d3cb2027
Sagar <sagargupta201@outlook.com>
2022-02-03 17:09:19 +0530

    Replace binding logic with proper symbols

---
+++ Grams/Code Analysis/VariableSymbol.cs
@@ -0,0 +1,14 @@
+﻿namespace Grams.Code_Analysis
+{
+    public sealed class VariableSymbol
+    {
+        internal VariableSymbol(string name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; }
+        public Type Type { get; }
+    }
+}
